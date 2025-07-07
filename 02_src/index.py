8e0c6b931f8314c3bb0afa7eb13346c9974fa65d
try-nishikawa <h-nishikawa@try-ltd.co.jp>
2025-07-07 14:36:36 +0900

    リンク追加

--- 02_src/index.py
+++ 02_src/index.py
@@ -7,7 +7,7 @@ def index_view(page: ft.Page):
             [
                 ft.Text("ドア開閉システム管理画面", style="headlineMedium"),
                 ft.ElevatedButton("カード管理", on_click=lambda e: page.go("/card")),
-                ft.ElevatedButton("ログ管理", on_click=lambda e: page.go("")),
+                ft.ElevatedButton("ログ管理", on_click=lambda e: page.go("/access_logs")),
                 ft.ElevatedButton("ログアウト", on_click=lambda e: page.go("/")),
             ],
         )
