63afc83f6ef398b770da537b459701c17e9ce1bf
hiromu-26 <h-takahashi@try-itd.co.jp>
2025-07-10 10:33:27 +0900

    実装中にログインを簡略化できるように変更

--- 02_src/my_app/app/views/login.py
+++ 02_src/my_app/app/views/login.py
@@ -5,9 +5,11 @@ import flet as ft
 def login(page: ft.Page):
     page.title = "ログイン画面"
 
-    username = ft.TextField(label="ユーザー名" , autofocus=True, on_submit=lambda e: password.focus())
+    username = ft.TextField(label="ユーザー名" , autofocus=True, on_submit=lambda e: page.go("/index"))
+    # username = ft.TextField(label="ユーザー名", autofocus=True, on_submit=lambda e: password.focus())
 
-    password = ft.TextField(label="パスワード", password=True, on_submit=lambda e: do_login(e))
+    password = ft.TextField(label="パスワード", password=True, on_submit=lambda e: page.go("/index"))
+    # password = ft.TextField(label="パスワード", password=True, on_submit=lambda e: do_login(e))
     msg = ft.Text("")
     def do_login(e):
         if username.value == "admin" and password.value == "password":
@@ -19,7 +21,8 @@ def login(page: ft.Page):
             msg.value = "ログイン失敗"
             page.update()
             username.focus()
-    login_btn = ft.ElevatedButton("ログイン", on_click=do_login)
+    login_btn = ft.ElevatedButton("ログイン", on_click=lambda e: page.go("/index"))
+    # login_btn = ft.ElevatedButton("ログイン", on_click=do_login)
 
     return ft.View(
             "/",
