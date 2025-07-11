08afe97968e7e8ebabc8e1a3844678013fc6ded8
hiromu-26 <h-takahashi@try-itd.co.jp>
2025-07-11 11:43:29 +0900

    画面のサイズ固定

--- 02_src/my_app/main.py
+++ 02_src/my_app/main.py
@@ -3,9 +3,9 @@ from app.views.router import route
 
 
 def main(page: ft.Page):
-    page.window_width = 800
-    page.window_height = 600
-    page.window_resizable = False
+    page.window.width = 1024
+    page.window.height = 768
+    page.window.resizable = False
     page.title = "ドア開閉システム"
 
     route(page)
