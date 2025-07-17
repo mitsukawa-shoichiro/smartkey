9ccccd592d1a5f0358bc0a82ac7c6f9e383ea046
try-nishikawa <h-nishikawa@try-ltd.co.jp>
2025-07-17 15:28:35 +0900

    ひとつ前の変更は検索ボタンのデザイン変更です。

--- 02_src/my_app/app/views/accesslogs.py
+++ 02_src/my_app/app/views/accesslogs.py
@@ -77,7 +77,6 @@ def accesslogs(page: ft.Page):
                 page.open(dialog)
                 return
         
-        
         logs = db.find_log(search_cardname, search_method, search_eventtype,start_dt,end_dt)
 
         table.rows.clear()
@@ -144,15 +143,48 @@ def accesslogs(page: ft.Page):
     end_date.on_change = change_end_date
 
     # ボタン定義
-    search_btn = ft.ElevatedButton("検索", on_click=search_logs)
-    show_all_btn = ft.ElevatedButton("全件表示", on_click=show_all_logs)
+    search_btn = ft.ElevatedButton(
+        text="検索",
+        on_click=search_logs,
+        width=80,
+        height=40,
+        bgcolor=ft.Colors.GREY_50,
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(
+                radius=0),
+            padding=ft.padding.all(0)
+        ),
+    )
+    show_all_btn = ft.ElevatedButton(
+        text="全件表示",
+        on_click=show_all_logs,
+        width=80,
+        height=40,
+        bgcolor=ft.Colors.GREY_50,
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(
+                radius=0),
+            padding=ft.padding.all(0)
+        ),
+    )
     startdate_btn = ft.ElevatedButton(text = "開始日時を選択", on_click=lambda e: open_datepicker(start_date))
     enddate_btn = ft.ElevatedButton(text = "終了日時を選択", on_click=lambda e: open_datepicker(end_date))
-    reset_btn = ft.ElevatedButton("検索欄をクリア", on_click=reset)
+    reset_btn = ft.ElevatedButton(
+        text="クリア",
+        on_click=reset,
+        width=80,
+        height=40,
+        bgcolor=ft.Colors.GREY_50,
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(
+                radius=0),
+            padding=ft.padding.all(0)
+        ),
+    )
     back_btn = ft.TextButton("🔙", on_click=lambda e: page.go("/index"),
                     style=ft.ButtonStyle(
                         padding=ft.padding.symmetric(horizontal=20, vertical=10),
-                        text_style=ft.TextStyle(size=20),
+                        text_style=ft.TextStyle(size=30),
                         shape=ft.RoundedRectangleBorder(radius=10),overlay_color=ft.Colors.BLUE_100,
                     ),
                 )
@@ -208,10 +240,16 @@ def accesslogs(page: ft.Page):
     search_area = ft.Container(
         content=ft.Column(
             [
-                ft.Row([searchcardname, searchmethod, searcheventtype], spacing=20),
-                ft.Row([ft.Text("開始日時:", width=80), startdate_btn, start_text], spacing=10),
-                ft.Row([ft.Text("終了日時:", width=80), enddate_btn, end_text], spacing=10),
-                ft.Row([search_btn,show_all_btn, reset_btn], alignment=ft.MainAxisAlignment.END, spacing=20),
+                ft.Row([searchcardname, searchmethod,
+                        ft.Container(searcheventtype, margin=ft.margin.only(right=30)),show_all_btn
+                ]),
+                ft.Row([startdate_btn,
+                        ft.Container(start_text, margin=ft.margin.only(right=98)),reset_btn
+                ]),
+                ft.Row([enddate_btn,
+                        ft.Container(end_text, margin=ft.margin.only(right=98)),search_btn
+                ])
+                # ft.Row([search_btn,show_all_btn, reset_btn], alignment=ft.MainAxisAlignment.END, spacing=20),
             ],
             spacing=15,
             horizontal_alignment=ft.CrossAxisAlignment.START
@@ -219,7 +257,7 @@ def accesslogs(page: ft.Page):
         padding=20,
         bgcolor=ft.Colors.GREY_200,
         border_radius=12,
-        width=800,
+        width=545,
         visible=False
     )
 
@@ -241,5 +279,5 @@ def accesslogs(page: ft.Page):
             scroll_table,
             back_btn,
         ],
-          padding=ft.Padding(left=100, top=20, right=0, bottom=20)
+          padding=ft.Padding(left=120, top=20, right=0, bottom=20)
     )
