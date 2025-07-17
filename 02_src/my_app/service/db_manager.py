cf30d3ea17eddd7afff8df8b1c5b2f42780efd0a
KeinoM438 <m-keino@try-ltd.co.jp>
2025-07-17 11:43:15 +0900

    '#'で記されたメモのほうでも挙動はします。

--- 02_src/my_app/service/db_manager.py
+++ 02_src/my_app/service/db_manager.py
@@ -18,21 +18,29 @@ def check_card(cardIDM):
         dict: カード情報（存在する場合）またはNone（存在しない場合）
     """
     conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
+   
     cursor = conn.cursor()
 
     try:
-        cursor.execute('SELECT * FROM card WHERE card_number = ?', (cardIDM,))
-        card = cursor.fetchone()
+        cursor.execute('SELECT card_id FROM card WHERE card_number = ?', (cardIDM,))
+        card_id = cursor.fetchone()
 
-        if card:
-            return True
-        else:
-            return False
+        # #if card_id:
+        #    # card_id=((str)(card_id))
+            
+        
+        #     print(card_id[0])#[1:len(card_id)-2])
+        #     print('成功です')
+        #     return card_id[0]
+        # else:
 
+        #     print(card_id[0])
+        #     print('失敗です')
+        #     return None
+        print(card_id[0])
     except Exception as e:
         print(f"カード検索エラー: {e}")
-        return False
+        print(None)
     finally:
         conn.close()
 
@@ -46,16 +54,16 @@ def insert_card_id(card_id):
     conn.close()
 
 
-def gainCardIDwithCardname(card_name):
+# def gainCardIDwithCardname(card_name):
 
-    # カードネームをもとに、カードIDを取得します。
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute("SELECT card_id FROM card WHERE card_name= ?", (card_name,))
-    card_id = cursor.fetchall()
-    conn.close()
-    print(card_id)
-    return card_id
+#     # カードネームをもとに、カードIDを取得します。
+#     conn = sqlite3.connect(DB_PATH)
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT card_id FROM card WHERE card_name= ?", (card_name,))
+#     card_id = cursor.fetchall()
+#     conn.close()
+#     print(card_id)
+#     return card_id
 
 
 def insert_card_id(card_id):
@@ -70,6 +78,11 @@ def insert_card_id(card_id):
 
 # endregion
 if __name__ == '__main__':
-
-    insert_card_id("1")
-    gain_card_id_with_card_name("SampleCard")
+ 
+    check_card(98765)
+    check_card(1234567890123456)
+    check_card(13579)
+    check_card(123)
+    check_card(45678)
+    check_card(321)
+    check_card(56789)
