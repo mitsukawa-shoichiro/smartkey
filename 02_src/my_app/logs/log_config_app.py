c8693bf3ecbf9d9a49a352c8f40f61e62e08d57e
hiromu-26 <h-takahashi@try-itd.co.jp>
2025-07-22 15:30:11 +0900

    外だしするにあたっての変更

--- 02_src/my_app/logs/log_config_app.py
+++ 02_src/my_app/logs/log_config_app.py
@@ -2,45 +2,20 @@
 log_config.py
 グローバルロギング初期化モジュール：プロジェクトのどこでも `import log_config` で利用可能。
 """
-import logging
-import logging.handlers
 import os
-import sys
-import queue
+import json
+import logging.config
 
-# ---- ログパスとファイル ----
-LOG_DIR = os.path.join(os.path.dirname(__file__))
-os.makedirs(LOG_DIR, exist_ok=True)
-LOG_PATH = os.path.join(LOG_DIR, "app.log")
+# 設定ファイルのパス
+BASE_DIR = os.path.dirname(__file__) + "\\..\\config"
+config_path = os.path.join(BASE_DIR, "app_log_config.json")
 
-# ---- フォーマッター ----
-FMT = "%(asctime)s [%(levelname)s] %(name)s:%(lineno)d - %(message)s"
-formatter = logging.Formatter(FMT, datefmt="%Y-%m-%d %H:%M:%S")
+with open(config_path, "r", encoding="utf-8") as f:
+    config = json.load(f)
 
-# ---- ファイルハンドラー（日次ローテーション）----
-file_hdl = logging.handlers.TimedRotatingFileHandler(
-    LOG_PATH, when="midnight", backupCount=10, encoding="utf-8"
-)
-file_hdl.setFormatter(formatter)
-file_hdl.setLevel(logging.INFO)
+logging.config.dictConfig(config)
 
-# ---- コンソールハンドラー ----
-console_hdl = logging.StreamHandler(sys.stdout)
-console_hdl.setFormatter(formatter)
-console_hdl.setLevel(logging.INFO)
-
-# # ---- ルートロガーの設定 ----
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-root.addHandler(file_hdl)
-root.addHandler(console_hdl)
-
-
-# ---- グローバル例外フック ----
-def _excepthook(exc_type, exc, tb):
-    if issubclass(exc_type, KeyboardInterrupt):
-        return sys.__excepthook__(exc_type, exc, tb)
-    root.critical("UNCAUGHT EXCEPTION", exc_info=(exc_type, exc, tb))
-
-
-sys.excepthook = _excepthook
+# テストログ
+logger = logging.getLogger(__name__)
+logger.debug("デバッグメッセージ")
+logger.info("情報メッセージ")
