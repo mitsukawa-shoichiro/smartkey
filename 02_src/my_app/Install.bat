3211efb05fbca6dfbbd4be5d4bd00ff575e715cb
LIUPENGJU <h-ryu@try-ltd.co.jp>
2025-07-29 16:57:20 +0900

    bat更新

--- 02_src/my_app/Install.bat
+++ 02_src/my_app/Install.bat
@@ -1,14 +1,11 @@
 @echo off
-rem install.bat  ─ 任何路径下双击即可
 
-rem ① 切到当前批处理文件所在目录
 pushd "%~dp0"
 
-rem ② 升级 pip（可选）
-python -m pip install --upgrade pip
+py -m pip install --upgrade pip
 
-rem ③ 安装依赖（全局 or --user 安装）
-python -m pip install -r requirements.txt --user
+py -m pip install -r requirements.txt --user
 
-rem ④ 返回原工作目录
-popd
+py service/windowsservice.py --startup delayed install
+
+pause
