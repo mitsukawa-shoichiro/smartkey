82a17b2f19f0ed19c4c671ba13c86fc4215ee467
hiromu-26 <h-takahashi@try-itd.co.jp>
2025-07-30 11:35:09 +0900

    正常にimportできるように

--- 02_src/my_app/Install.bat
+++ 02_src/my_app/Install.bat
@@ -4,7 +4,7 @@ pushd "%~dp0"
 
 py -m pip install --upgrade pip
 
-py -m pip install -r requirements.txt --user
+py -m pip install -r requirements.txt
 
 py service/windowsservice.py --startup delayed install
 
