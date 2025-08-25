57a12e65b840dee98a536771403552d32f63e77d
hiromu-26 <h-takahashi@try-itd.co.jp>
2025-08-25 15:58:57 +0900

    修正

--- 02_src/my_app/service/utils/usb_card_readers.py
+++ 02_src/my_app/service/utils/usb_card_readers.py
@@ -33,9 +33,12 @@ def get_all_serials():
         vid = info["vid"]
         pid = info["pid"]
         s = info["serial"]
-
-        serial_regex = re.escape(s)
-
+        if s.isdigit():
+            serial_regex = rf"\d{{{len(s)}}}"
+        else:
+            # 使用されている文字だけを許容する
+            serial_regex = "[" + "".join(sorted(set(s))) + "]+"
+        print(serial_regex)
         wql = f"SELECT DeviceID FROM Win32_PnPEntity WHERE DeviceID LIKE '%VID_{vid}&PID_{pid}%'"
         pattern = re.compile(
             rf"USB\\VID_{vid}&PID_{pid}\\({serial_regex})$", re.IGNORECASE)
